d4e509b608631afaadfdae966f63e6cc9c75b57b
ShenXu <sxbczyh2134@163.com>
2021-11-17 23:34:53 +0800

    axios的二次封装1（测试）

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Header/>
+    <button @click="send">点击发送请求</button>
     <router-view></router-view>
     <Footer/>
   </div>
@@ -9,9 +10,18 @@
 <script>
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+import ajax from '@/api/network'
+
   export default {
     name:'App',
-    components:{Header, Footer}
+    components:{Header, Footer},
+    methods:{
+      async send(){
+       let res = await ajax.get('/hitokoto/get?type=social')
+       console.log(res)
+      }
+    }
   }
 </script>
 
---
+++ src/api/network.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+// 导入NProgress包（进度条）对应的js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 进行一些全局的配置
+const ajax = axios.create({
+  baseURL:'https://api.uixsj.cn',
+  timeout: 5000
+})
+
+// 添加请求拦截器
+ajax.interceptors.request.use(
+    (config)=>{
+      NProgress.start()
+      return config
+  },
+    (error)=>{
+      return Promise.reject(error)
+  })
+
+  // 添加响应拦截器
+  ajax.interceptors.response.use(
+    (response)=>{
+      NProgress.done()
+      return response.data
+    },
+    (error) =>{
+      return Promise.reject(error)
+    }
+  )
+
+  export default ajax
